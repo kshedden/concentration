ad82f2999b32b4efacd13c417cf48ab32c07aa6a
Kerby Shedden <kshedden@umich.edu>
2021-09-11 17:24:15 -0400

    update

--- functional_lr.jl
+++ functional_lr.jl
@@ -2,7 +2,9 @@ using Optim, LinearAlgebra, IterTools, Statistics, Random
 using UnicodePlots # for testing only
 
 # Construct a smoothing penalty matrix for a vector of length p.
-function _fw(p)
+# Each row contains the discreteized second derivative at a 
+# specific position.
+function _fpen(p)
     a = zeros(p-2, p)
     for i in 1:p-2
         a[i, i:i+2] = [1 -2 1]
@@ -24,8 +26,8 @@ function _split(z, p, q)
     return tuple(u, v)
 end
 
-
-# Return the objective function used to fit the model and its corresponding gradient function.
+# Return the objective function and corresponding gradient functions
+# that are used to fit the low rank model.
 function _flr_fungrad_tensor(x::Array{Float64,1}, p::Int, r::Int, cu::Array{Float64,1}, cv::Array{Float64,1})
 
     @assert length(x) == p^r
@@ -34,7 +36,7 @@ function _flr_fungrad_tensor(x::Array{Float64,1}, p::Int, r::Int, cu::Array{Floa
     q = r - 1
 
     # The size of the tensor representation of the data along each axis
-    di = p * ones(Int, r)
+	di = fill(p, r)
 
     # Convert the data to a tensor
     xr = reshape(x, di...)
@@ -44,28 +46,25 @@ function _flr_fungrad_tensor(x::Array{Float64,1}, p::Int, r::Int, cu::Array{Floa
     rs = zeros(di...)
 
     # The smoothing penalty matrix.
-    w = _fw(p)
+    w = _fpen(p)
     w2 = w' * w
 
-    # Fitted values
+    # Set the fitted values and residuals
     function _getfit(u, v)
-        ft .= 0
+        ft .= 0 # Fitted values
         for ii in product(Iterators.repeated(1:p, r)...)
             for k in 1:q
                 ft[ii...] += u[ii[k], k] * v[ii[end], k]
             end
         end
+        rs .= xr .- ft # Residuals
     end
 
     # The fitting function (sum of squared residuals).
     f = function(z::Array{Float64,1})
 
         u, v = _split(z, p, q)
-
         _getfit(u, v)
-
-        # Residuals
-        rs .= xr .- ft
 
         # Sum of squared residuals
         rv = sum(abs2, rs)
@@ -77,7 +76,6 @@ function _flr_fungrad_tensor(x::Array{Float64,1}, p::Int, r::Int, cu::Array{Floa
         end
 
         return rv
-
     end
 
     # The gradient of the fitting function
@@ -87,9 +85,6 @@ function _flr_fungrad_tensor(x::Array{Float64,1}, p::Int, r::Int, cu::Array{Floa
         u, v = _split(z, p, q)
 
         _getfit(u, v)
-
-        # Residuals
-        rs .= xr .- ft
 
         # Sum of squared residuals
         rv = sum(abs2, rs)
@@ -194,25 +189,6 @@ function get_start(x::Array{Float64,1}, p::Int, r::Int)
 
 end
 
-function check_get_start()
-
-    p = 10
-    r = 4
-    q = r - 1
-    s = 1.0
-
-    x, u, v = gen_tensor(p, r, s)
-    uh, vh = get_start(x[:], p, r)
-
-    for j in 1:q
-        m1 = u[:, j] * v[:, j]'
-        m2 = uh[:, j] * vh[:, j]'
-        println(cor(vec(m1), vec(m2)))
-    end
-
-end
-
-
 # Fit a functional low-rank model to the tensor x.  The input data x is a flattened representation
 # of a tensor with r axes, each having length p.
 function fit_flr_tensor(x::Array{Float64,1}, p::Int, r::Int, cu::Array{Float64,1}, cv::Array{Float64,1}; start=nothing)
@@ -246,126 +222,14 @@ function fit_flr_tensor(x::Array{Float64,1}, p::Int, r::Int, cu::Array{Float64,1
 end
 
 
-# Simulate data for testing.  The data are a tensor with r
-# axes, each with length p.  The additive Gaussian error has
-# standard deviation s.
-function gen_tensor(p::Int, r::Int, s::Float64)
-
-    q = r - 1
-    di = p * ones(Int, r)
-    u = zeros(p, q)
-    v = zeros(p, q)
-    grid = range(-1, 1, length=p)
-
-    for j in 1:q
-        ic, sl = randn(), randn()
-        u[:, j] = ic .+ sl*grid
-        ic, sl = randn(), randn()
-        v[:, j] = ic .+ sl*grid
-
-        # Center to make the representation identified
-        if j > 1
-            u[:, j] .-= mean(u[:, j])
-        end
-
-        # Scale to make the representation identified
-        f = norm(u[:, j])
-        u[:, j] /= f
-        v[:, j] *= f
-
-    end
-
-    x = zeros(Float64, di...)
-
-    for ii in product(Iterators.repeated(1:p, r)...)
-        for k in 1:q
-            x[ii...] += u[ii[k], k] * v[ii[end], k]
-        end
-    end
-
-    x += s * randn(di...)
-
-    return x, u, v
-
-end
-
-function check_grad_tensor_helper(;p=10, r=4, s=1.0, e=1e-5, pu=10.0, pv=10.0)
-
-    x, u, v = gen_tensor(p, r, s)
-
-    di = p*ones(Int, r)
-    q = r - 1
-    cu = pu * ones(q)
-    cv = pv * ones(q)
-
-    f, g! = _flr_fungrad_tensor(x[:], p, r, cu, cv)
-
-    # Test the gradient at this point
-    u0 = u + randn(p, q)
-    v0 = v + randn(p, q)
-
-    # Get the numerical gradient
-    pa = vcat(u0[:], v0[:])
-    f0 = f(pa)
-    ng = zeros(length(pa))
-    for i in eachindex(pa)
-        pa[i] += e
-        f1 = f(pa)
-        ng[i] = (f1 - f0) / e
-        pa[i] -= e
-    end
-
-    # Get the analytic gradient
-    ag = zeros(length(pa))
-    g!(ag, pa, project=false)
-
-    d = maximum(abs.((ag - ng) ./ abs.(ng)))
-    @assert d < 0.005
-
-end
-
-function check_grad_tensor()
-
-    Random.seed!(3942)
-
-    for r in [2, 4]
-        for pe in [0.0, 5.0, 20.0]
-            for j in 1:5
-                check_grad_tensor_helper(p=10, r=r, s=1.0, e=1e-5, pu=pe, pv=pe)
-            end
-        end
-    end
-
-end
-
-function check_fit_tensor(; p=10, r=4, s=1.0)
-
-    s = 0.5
-
-    # Number of covariates
-    q = r - 1
-
-    x, u, v = gen_tensor(p, r, s)
-
-    # All parameters flattened
-    pa = vcat(u[:], v[:])
-
-    # Increasing levels of regularization
-    for c in [100, 10000]
-        cu = c * ones(q)
-        cv = c * ones(q)
-        uh, vh = fit_flr_tensor(x[:], p, r, cu, cv)
-
-        for j in 1:q
-            m1 = u[:, j] * v[:, j]'
-            m2 = uh[:, j] * vh[:, j]'
-            plt = scatterplot(vec(m1), vec(m2))
-            println(plt)
-        end
-    end
-
-end
-
+# Reparametrize the model from the form that is directly
+# fit to the data, to a form that is more interpretable.
+# The form that is directly fit to the data is u*v'.
+# The form produced here takes the fitted value
+# at the central position of u, denoted 'mu', and 
+# adjusts u accordingly to produce new scores uu.
+# The fitted values are identical: u*v' = mu + uu*v'.
+# Note that v is not changed by the reparameterization.
 function reparameterize(u, v)
 
     @assert size(u, 1) == size(v, 1)
@@ -374,49 +238,17 @@ function reparameterize(u, v)
     u = copy(u)
     v = copy(v)
 
+	# The central position of the u axis.
     m = div(p, 2)
-    mu = zeros(p)
+
+    # The central value that is split out from
+    # the factor representation.
+    mu = v * u[m, :]
+    
     for j in 1:q
-        mu += u[m, j] * v[:, j]
         u[:, j] .-= u[m, j]
     end
 
     return tuple(mu, u, v)
 
 end
-
-function check_reparameterize()
-
-    p, q = 10, 4
-    r = q + 1
-    u = randn(p, q)
-    v = randn(p, q)
-
-    mu, u1, v1 = reparameterize(u, v)
-
-    # Build a tensor from the first parameterization.
-    di = p * ones(Int, r)
-    x1 = zeros(di...)
-    for ii in product(Iterators.repeated(1:p, r)...)
-        for k in 1:q
-            x1[ii...] += u[ii[k], k] * v[ii[end], k]
-        end
-    end
-
-    # Build a tensor from the second parameterization.
-    x2 = zeros(di...)
-    for ii in product(Iterators.repeated(1:p, r)...)
-        x2[ii...] = mu[ii[end]]
-        for k in 1:q
-            x2[ii...] += u1[ii[k], k] * v1[ii[end], k]
-        end
-    end
-
-    @assert maximum(abs.(x1 - x2)) < 1e-10
-
-end
-
-#check_reparameterize()
-#check_fit_tensor()
-#check_get_start()
-#check_grad_tensor()
---
+++ test_functional_lr.jl
@@ -0,0 +1,190 @@
+include("functional_lr.jl")
+
+# Simulate data for testing.  The data are a tensor with r
+# axes, each with length p.  The additive Gaussian error has
+# standard deviation s.
+function gen_tensor(p::Int, r::Int, s::Float64)
+
+    q = r - 1
+    di = fill(p, r)
+    u = zeros(p, q)
+    v = zeros(p, q)
+
+    # Generate data with a random intercept and random
+    # slope on this grid.
+    grid = range(-1, 1, length=p)
+
+    for j in 1:q
+        ic, sl = randn(), randn()
+        u[:, j] = ic .+ sl*grid
+        ic, sl = randn(), randn()
+        v[:, j] = ic .+ sl*grid
+
+        # Center to make the representation identified
+        if j > 1
+            u[:, j] .-= mean(u[:, j])
+        end
+
+        # Scale to make the representation identified
+        f = norm(u[:, j])
+        u[:, j] /= f
+        v[:, j] *= f
+
+    end
+
+    x = zeros(Float64, di...)
+    for ii in product(Iterators.repeated(1:p, r)...)
+        for k in 1:q
+            x[ii...] += u[ii[k], k] * v[ii[end], k]
+        end
+    end
+
+	# Additive noise
+    x += s * randn(di...)
+
+    return x, u, v
+
+end
+
+function check_reparameterize()
+
+    p, q = 10, 4
+    r = q + 1
+    u = randn(p, q)
+    v = randn(p, q)
+
+    mu, u1, v1 = reparameterize(u, v)
+
+    # Build a tensor from the first parameterization.
+    di = fill(p, r)
+    x1 = zeros(di...)
+    for ii in product(Iterators.repeated(1:p, r)...)
+        for k in 1:q
+            x1[ii...] += u[ii[k], k] * v[ii[end], k]
+        end
+    end
+
+    # Build a tensor from the second parameterization.
+    x2 = zeros(di...)
+    for ii in product(Iterators.repeated(1:p, r)...)
+        x2[ii...] = mu[ii[end]]
+        for k in 1:q
+            x2[ii...] += u1[ii[k], k] * v1[ii[end], k]
+        end
+    end
+
+    @assert maximum(abs.(x1 - x2)) < 1e-10
+
+end
+
+# This test is stochastic, but if the noise
+# parameter is small it almost always passes.
+function check_get_start()
+
+    p = 10 # Number of grid points
+    q = 3 # Number of covariates
+    s = 0.01 # Additive noise
+
+    # Rank of the tensor
+    r = q + 1
+
+	for i in 1:10
+
+        x, u, v = gen_tensor(p, r, s)
+        uh, vh = get_start(x[:], p, r)
+
+        for j in 1:q
+        	# Correlate true and estimated values for
+    	    # each component.
+            m1 = u[:, j] * v[:, j]'
+            m2 = uh[:, j] * vh[:, j]'
+            @assert cor(vec(m1), vec(m2)) > 0.95
+        end
+    end
+
+end
+
+function check_grad_tensor_helper(;p=10, r=4, s=1.0, e=1e-5, pu=10.0, pv=10.0)
+
+    x, u, v = gen_tensor(p, r, s)
+
+    di = p*ones(Int, r)
+    q = r - 1
+    cu = pu * ones(q)
+    cv = pv * ones(q)
+
+    f, g! = _flr_fungrad_tensor(x[:], p, r, cu, cv)
+
+    # Test the gradient at this point
+    u0 = u + randn(p, q)
+    v0 = v + randn(p, q)
+
+    # Get the numerical gradient
+    pa = vcat(u0[:], v0[:])
+    f0 = f(pa)
+    ng = zeros(length(pa))
+    for i in eachindex(pa)
+        pa[i] += e
+        f1 = f(pa)
+        ng[i] = (f1 - f0) / e
+        pa[i] -= e
+    end
+
+    # Get the analytic gradient
+    ag = zeros(length(pa))
+    g!(ag, pa, project=false)
+
+    d = maximum(abs.((ag - ng) ./ abs.(ng)))
+    @assert d < 0.005
+
+end
+
+function check_grad_tensor()
+
+    Random.seed!(3942)
+
+    for r in [2, 4]
+        for pe in [0.0, 5.0, 20.0]
+            for j in 1:5
+                check_grad_tensor_helper(p=10, r=r, s=1.0, e=1e-5, pu=pe, pv=pe)
+            end
+        end
+    end
+
+end
+
+function check_fit_tensor(; p=10, r=4, s=1.0)
+
+    s = 0.5
+
+    # Number of covariates
+    q = r - 1
+
+    x, u, v = gen_tensor(p, r, s)
+
+    # All parameters flattened
+    pa = vcat(u[:], v[:])
+
+    # Increasing levels of regularization
+    for c in [100, 10000]
+        cu = c * ones(q)
+        cv = c * ones(q)
+        uh, vh = fit_flr_tensor(x[:], p, r, cu, cv)
+
+        for j in 1:q
+            m1 = u[:, j] * v[:, j]'
+            m2 = uh[:, j] * vh[:, j]'
+            plt = scatterplot(vec(m1), vec(m2))
+            println(plt)
+        end
+    end
+
+end
+
+# Fast tests
+check_reparameterize()
+check_get_start()
+
+# Slow tests
+check_fit_tensor()
+check_grad_tensor()
