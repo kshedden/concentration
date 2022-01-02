bfdc50e6c21d7951a2b38d1d567085031458f35b
Kerby Shedden <kshedden@umich.edu>
2022-01-01 22:32:45 -0500

    update

--- procmodel.jl
+++ procmodel.jl
@@ -26,16 +26,21 @@ end
 
 x = ones(size(dx, 1), 2)
 x[:, 2] = dx[:, :Age_Yrs][:, :]
-xm = Xmat(xb, x, x, x)
+xm = Xmat(xb, x, x, x[:, 1:1])
 
-pm = ProcessMLEModel(y, xm, age, idv)#; fix_unexplained=[1.0, 0])
+pm = ProcessMLEModel(y, xm, age, idv, fix_unexplained=[1.0])
+pm.penalty = Penalty(1000, 1000, 1000, 0)
 fit!(pm; verbose=true)
 
+pm.penalty = Penalty(1000, 100, 100, 0)
+fit!(pm, start=pm.params, verbose=true)
+
+error("")
 # Fitted mean curve
 ff = xb * coef(pm)[1:6]
 scatterplot(age, ff)
 
-pme = emulate(pm.params; X=pm.X, grp=pm.grplab, time=pm.time)
+pme = emulate(pm.params; X=pm.X, grp=pm.grplab, time=pm.time, penalty=penalty)
 #pme.fix_unexplained = pm.fix_unexplained
 
 fit!(pme, verbose=true)
